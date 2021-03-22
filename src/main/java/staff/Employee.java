e2fb526b18f662773c67cd3ee8b5b7189e53eb5e
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-03-22 14:26:20 +0000

    manager class added and tested

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,35 @@
+package staff;
+
+public  abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private double salary;
+
+    public Employee(String name, String niNumber, double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNiNumber() {
+        return niNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public double raiseSalary(double increment){
+       return salary + increment;
+
+    }
+
+    public double payBonus(){
+        return salary /100;
+    }
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("John", "JC123456A", 30000.00, "Technical Team" );
+    }
+
+    @Test
+    public void managerHasAName(){
+        assertEquals("John", manager.getName());
+    }
+
+    @Test
+    public void managerHasAnNiNumber(){
+        assertEquals("JC123456A", manager.getNiNumber());
+    }
+
+    @Test
+    public void managerHasASalary(){
+        assertEquals(30000.00, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void managerHasADeptName(){
+        assertEquals("Technical Team", manager.getDeptName());
+    }
+}
